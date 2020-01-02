927e947fe8164c15f64e10695d21b5d60f83da3f
JasonMacKeigan <jay_6955@hotmail.com>
2020-01-02 12:13:26 -0400

    PlayerStoreResponse is now immutable.

--- src/main/java/com/teamgames/gamepayments/PlayerStoreResponse.java
+++ src/main/java/com/teamgames/gamepayments/PlayerStoreResponse.java
@@ -2,8 +2,13 @@ package com.teamgames.gamepayments;
 
 public class PlayerStoreResponse {
 	
-	String message;
-	String extendedMessage;
+	private final String message;
+	private final String extendedMessage;
+
+	public PlayerStoreResponse(String message, String extendedMessage) {
+		this.message = message;
+		this.extendedMessage = extendedMessage;
+	}
 
 	public String getMessage() {
 		return message;
